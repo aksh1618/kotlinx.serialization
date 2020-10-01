4d04275d49ff1f49628c131916c1bf98dcfb17dc
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2020-10-01 14:21:08 +0300

    Fix deserialization of float and double types in CBOR (#1034)
      * add support of half-precision floating-point values for double and float decoders
      * double decoder now can read float values
      * write tests for boundary and other values

--- formats/cbor/jvmTest/src/kotlinx/serialization/cbor/CborCompatibilityTest.kt
+++ formats/cbor/jvmTest/src/kotlinx/serialization/cbor/CborCompatibilityTest.kt
@@ -19,9 +19,12 @@ class CborCompatibilityTest {
     data class WithMap(val map: Map<Long, Long>)
     @Serializable
     data class IntData(val intV: Int)
-
     @Serializable
     data class StringData(val data: String)
+    @Serializable
+    data class FloatData(val field: Float)
+    @Serializable
+    data class DoubleData(val field: Double)
 
     @Serializable
     data class SomeComplexClass<T>(
@@ -75,5 +78,43 @@ class CborCompatibilityTest {
         )
         val serial = SomeComplexClass.serializer(ListSerializer(Int.serializer()))
         compare(obj, serial)
+    }
+
+    @Test
+    fun testFloat() {
+        compare(FloatData(Float.NaN), FloatData.serializer())
+        compare(FloatData(Float.POSITIVE_INFINITY), FloatData.serializer())
+        compare(FloatData(Float.NEGATIVE_INFINITY), FloatData.serializer())
+        compare(FloatData(Float.MAX_VALUE), FloatData.serializer())
+        compare(FloatData(Float.MIN_VALUE), FloatData.serializer())
+        compare(FloatData(0.0f), FloatData.serializer())
+        compare(FloatData(-0.0f), FloatData.serializer())
+        compare(FloatData(-1.0f), FloatData.serializer())
+        compare(FloatData(1.0f), FloatData.serializer())
+        compare(FloatData(123.56f), FloatData.serializer())
+        compare(FloatData(123.0f), FloatData.serializer())
+        // minimal denormalized value in half-precision
+        compare(FloatData(5.9604645E-8f), FloatData.serializer())
+        // maximal denormalized value in half-precision
+        compare(FloatData(0.000060975552f), FloatData.serializer())
+    }
+
+    @Test
+    fun testDouble() {
+        compare(DoubleData(Double.NaN), DoubleData.serializer())
+        compare(DoubleData(Double.POSITIVE_INFINITY), DoubleData.serializer())
+        compare(DoubleData(Double.NEGATIVE_INFINITY), DoubleData.serializer())
+        compare(DoubleData(Double.MAX_VALUE), DoubleData.serializer())
+        compare(DoubleData(Double.MIN_VALUE), DoubleData.serializer())
+        compare(DoubleData(0.0), DoubleData.serializer())
+        compare(DoubleData(-0.0), DoubleData.serializer())
+        compare(DoubleData(-1.0), DoubleData.serializer())
+        compare(DoubleData(1.0), DoubleData.serializer())
+        compare(DoubleData(123.56), DoubleData.serializer())
+        compare(DoubleData(123.0), DoubleData.serializer())
+        // minimal denormalized value in half-precision
+        compare(DoubleData(5.9604644775390625E-8), DoubleData.serializer())
+        // maximal denormalized value in half-precision
+        compare(DoubleData(0.00006097555160522461), DoubleData.serializer())
     }
 }
